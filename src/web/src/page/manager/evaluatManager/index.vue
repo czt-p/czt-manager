1fe5e182f6c4dbbe53ea6abcd2fe824be0200727
modalone <840455798@qq.com>
2019-01-07 15:26:28 +0800

    评测管理

--- src/web/src/page/manager/evaluatManager/index.vue
+++ src/web/src/page/manager/evaluatManager/index.vue
@@ -101,7 +101,7 @@
     },
     methods:{
       handleClick(tab, event) {
-        console.log(tab, event);
+        // console.log(tab, event);
       },
       searchTable(){
         let param = {
